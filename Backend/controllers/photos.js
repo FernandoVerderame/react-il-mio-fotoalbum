0c03cee28311786dc28876ceb843a83b2a5ab60d
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-06-26 14:58:44 +0200

    added crud photo controllers

--- Backend/controllers/photos.js
+++ Backend/controllers/photos.js
@@ -1,26 +1,197 @@
+// Importo PrismaClient
+const { PrismaClient } = require("@prisma/client");
+
+// Inizializzo Prisma
+const prisma = new PrismaClient();
+
+// Importo la funzione per generare lo slug
+const createSlug = require("../utils/slug.js");
+
 // Store
 const store = async (req, res) => {
 
+    const { title, image, description, categories } = req.body;
+
+    // Genero lo slug
+    const slug = createSlug(title);
+
+    const data = {
+        title,
+        slug: slug,
+        image: image ? image : '',
+        description,
+        visible: req.body.visible ? true : false,
+        categories: {
+            connect: categories.map(id => ({ id: parseInt(id) }))
+        }
+    }
+
+    try {
+        const photo = await prisma.photo.create({
+            data
+        });
+        res.status(200).send(photo);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Server Error");
+    }
 }
 
 // Index
 const index = async (req, res) => {
+    try {
+        const where = {};
+        const { visible, title, page = 1, limit = 9 } = req.query;
+
+        // Filtro pubblicato
+        if (visible === 'true') {
+            where.visible = true
+        } else if (visible === 'false') {
+            where.visible = false
+        }
+
+        // Filtro per titolo
+        if (title) {
+            where.title = { contains: title }
+        }
+
+        // Paginazione
+        const offset = (page - 1) * limit;
+
+        const totalItems = await prisma.photo.count({ where });
+        const totalPages = Math.ceil(totalItems / limit);
+
+        if (page > totalPages) {
+            throw new Error(`La pagina ${page} non esiste.`);
+        }
+
+        const photos = await prisma.photo.findMany({
+            where,
+            orderBy: [
+                {
+                    createdAt: 'desc'
+                }
+            ],
+            include: {
+                categories: {
+                    select: {
+                        id: true,
+                        name: true,
+                        color: true
+                    }
+                },
+                user: {
+                    select: {
+                        id: true,
+                        name: true
+                    }
+                }
+            },
+            take: parseInt(limit),
+            skip: parseInt(offset)
+        });
+        res.json({
+            data: photos,
+            page,
+            totalItems,
+            totalPages
+        });
+    } catch (err) {
+        console.error(err);
+    }
 
 }
 
 // Show
 const show = async (req, res) => {
+    try {
+        const { slug } = req.params;
+        const photo = await prisma.photo.findUnique({
+            where: { slug },
+            include: {
+                categories: {
+                    select: {
+                        id: true,
+                        name: true
+                    }
+                },
+                user: {
+                    select: {
+                        id: true,
+                        name: true
+                    }
+                }
+            }
+        });
 
+        if (photo) {
+            res.json(photo);
+        } else {
+            throw new Error(`Foto con slug: ${slug} non trovata.`, 404);
+        }
+
+    } catch (err) {
+        console.error(err);
+    }
 }
 
 // Update
 const update = async (req, res) => {
+    try {
+        const { slug } = req.params;
+        const { title, description, image, categories } = req.body;
 
+        // Genero lo slug
+        const newSlug = createSlug(title);
+
+        const data = {
+            title,
+            slug: newSlug,
+            image: image ? image : '',
+            description,
+            visible: req.body.visible ? true : false,
+            categories: {
+                set: categories.map(id => ({ id: parseInt(id) }))
+            }
+        }
+
+        const photo = await prisma.photo.update({
+            where: { slug },
+            data,
+            include: {
+                categories: {
+                    select: {
+                        id: true,
+                        name: true
+                    }
+                },
+                user: {
+                    select: {
+                        id: true,
+                        name: true
+                    }
+                }
+            }
+        });
+        res.json(photo);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Server Error");
+    }
 }
 
 // Destroy
 const destroy = async (req, res) => {
-
+    try {
+        const { slug } = req.params;
+        await prisma.photo.delete({
+            where: { slug }
+        });
+        res.json(`Foto con slug: ${slug} eliminato con successo.`);
+    } catch (err) {
+        // next(err);
+        console.error(err);
+    }
 }
 
 module.exports = {
