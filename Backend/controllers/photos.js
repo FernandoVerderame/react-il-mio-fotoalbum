b4fffc84e3f888496a90de7516a84cdb7bc46804
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-06-26 10:55:24 +0200

    created photos controller

---
+++ Backend/controllers/photos.js
@@ -0,0 +1,32 @@
+// Store
+const store = async (req, res) => {
+
+}
+
+// Index
+const index = async (req, res) => {
+
+}
+
+// Show
+const show = async (req, res) => {
+
+}
+
+// Update
+const update = async (req, res) => {
+
+}
+
+// Destroy
+const destroy = async (req, res) => {
+
+}
+
+module.exports = {
+    store,
+    index,
+    show,
+    update,
+    destroy
+}
