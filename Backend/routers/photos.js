65a1671843711474693b788a257134b650e9c745
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-06-26 10:55:46 +0200

    added photos routes

--- Backend/routers/photos.js
+++ Backend/routers/photos.js
@@ -4,4 +4,28 @@ const express = require("express");
 // Istanza di express.Router()
 const router = express.Router();
 
+// Importo il controller delle foto
+const {
+    store,
+    index,
+    show,
+    update,
+    destroy
+} = require("../controllers/photos.js");
+
+// Rotta index
+router.get('/', index);
+
+// Rotta show
+router.get('/:slug', show);
+
+// Rotta store
+router.post('/', store);
+
+// Rotta update
+router.put('/:slug', update);
+
+//Rotta destroy
+router.delete('/:slug', destroy);
+
 module.exports = router;
