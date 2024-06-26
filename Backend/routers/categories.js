f63d934a45fd6000557eca5ceedb943341006967
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-06-26 16:25:24 +0200

    added comment into categories router

--- Backend/routers/categories.js
+++ Backend/routers/categories.js
@@ -4,7 +4,7 @@ const express = require("express");
 // Istanza di express.Router()
 const router = express.Router();
 
-// Importo le funzioni dei Posts
+// Importo le funzioni delle categorie
 const {
     store,
     index,
@@ -13,14 +13,19 @@ const {
     destroy
 } = require("../controllers/categories.js");
 
+// Rotta store
 router.post('/', store);
 
+// Rotta index
 router.get('/', index);
 
+// Rotta show
 router.get('/:id', show);
 
+// Rotta update
 router.put('/:id', update);
 
+// Rotta destroy
 router.delete('/:id', destroy);
 
 module.exports = router;
