8ac1a3b2fe187e8793bad206417849eabc094f5e
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-06-27 13:31:59 +0200

    updated photo card layout

--- Frontend/src/components/PhotoCard/PhotoCard.jsx
+++ Frontend/src/components/PhotoCard/PhotoCard.jsx
@@ -1,42 +1,63 @@
 import photoCardStyle from './PhotoCard.module.scss';
 
-const PostCard = ({ title, image, description, categories, user }) => {
-
-    const abstract = () => description ? description.slice(0, 60) + '...' : '';
+const PostCard = ({ title, image, description, categories, user, isShow }) => {
 
     return (
-        <div className={photoCardStyle.photoCard}>
+        <section id="photo-card">
 
-            <div className={photoCardStyle.image}>
-                <img src={image ? image : "https://placehold.co/600x400"} alt={title} className={photoCardStyle.img} />
-            </div>
+            <div className={photoCardStyle.photoCard}>
 
-            <div className={photoCardStyle.bottom}>
-                <div className="d-flex justify-content-between align-items-center mb-2">
-                    <h3 className="h5">{title}</h3>
+                <div className="thumb d-flex justify-content-center pt-5">
+                    <div className={photoCardStyle.image}>
+                        <img src={image ? image : "https://placehold.co/600x400"} alt={title} className={photoCardStyle.img} />
+                    </div>
                 </div>
 
-                <p className={photoCardStyle.paragraph}>{abstract()}</p>
+                <div className={photoCardStyle.bottom}>
+                    <div className="">
+                        <h3 className="h5">{title}</h3>
+                    </div>
 
-                <div className={photoCardStyle.dFlex}>
+                    <div className={photoCardStyle.dFlex}>
 
-                    {categories?.length > 0 ? (
-                        <div>
-                            <ul className={photoCardStyle.categories}>
-                                {categories.map((category, i) => (
-                                    <li key={`category-${i}`} style={{ backgroundColor: category.color || '#ccc' }} className={photoCardStyle.photoBadge}>
-                                        {category.name}
-                                    </li>
-                                ))}
-                            </ul>
-                        </div>
-                    ) : (
-                        <p>Nessuna categoria</p>
+
+                    </div>
+                    <p className="mb-0 fst-italic">{user?.name}</p>
+                </div>
+            </div>
+
+            <div className="photo-info">
+
+                <div className="photo-desc">
+                    {isShow && (
+                        <p className={photoCardStyle.paragraph}>{description}</p>
                     )}
                 </div>
-                <p className="mt-3 mb-0 fst-italic text-center">{user?.name}</p>
+
+                <div className="photo-cat">
+                    {isShow && (
+                        <div>
+                            {categories?.length > 0 ? (
+                                <div>
+                                    <ul className={photoCardStyle.categories}>
+                                        {categories.map((category, i) => (
+                                            <li key={`category-${i}`} style={{ backgroundColor: category.color || '#ccc' }} className={photoCardStyle.photoBadge}>
+                                                {category.name}
+                                            </li>
+                                        ))}
+                                    </ul>
+                                </div>
+                            ) : (
+                                <p>Nessuna categoria</p>
+                            )}
+                        </div>
+                    )}
+                </div>
+
             </div>
-        </div>
+
+        </section>
+
     );
 }
 
