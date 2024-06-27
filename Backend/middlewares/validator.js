caac2cbabc4dbd2d42167e62c26980f55642dfd9
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-06-27 11:08:30 +0200

    added delete pic into validator

--- Backend/middlewares/validator.js
+++ Backend/middlewares/validator.js
@@ -1,4 +1,5 @@
 const { checkSchema, validationResult } = require("express-validator");
+const deletePic = require("../utils/deletePic.js");
 
 const validator = (schema) => {
     return [
@@ -6,6 +7,9 @@ const validator = (schema) => {
         (req, res, next) => {
             const errors = validationResult(req);
             if (!errors.isEmpty()) {
+                if (req.file) {
+                    deletePic('photo_pics', req.file.filename);
+                }
                 return res.status(400).json({ errors: errors.array() })
             }
             next();
