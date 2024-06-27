4e7a5bd5ee415c315516eb6ef9ed39777d1a15ff
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-06-27 09:52:18 +0200

    added autentication token

---
+++ Backend/middlewares/authToken.js
@@ -0,0 +1,23 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+const authToken = (req, res, next) => {
+
+    const authHeader = req.headers.authorization;
+    const token = authHeader && authHeader.split(" ")[1];
+
+    if (!token) {
+        throw new Error('Token non fornito!', 401);
+    }
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, data) => {
+        if (err) {
+            throw new Error('Token non valido!', 403);
+        }
+        req.user = data;
+        next();
+    });
+
+}
+
+module.exports = authToken;
