d04f84001e1db003a7414c155dab05082dccafa0
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-06-27 18:55:54 +0200

    added new photo button

--- Frontend/src/pages/Photos.jsx
+++ Frontend/src/pages/Photos.jsx
@@ -1,6 +1,7 @@
 import axios from "../utils/axiosClient.js";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { MdOutlineAddAPhoto as AddPhoto } from "react-icons/md";
 import PhotoCard from "../components/PhotoCard/PhotoCard.jsx";
 
 const Photos = () => {
@@ -21,6 +22,7 @@ const Photos = () => {
         <section className="container my-5">
             <div className="d-flex justify-content-between align-items-center mb-5">
                 <h1 className="m-0">Album</h1>
+                <Link to="create" className="btn btn-primary">Nuova <AddPhoto /></Link>
             </div>
             <div className="row g-5">
                 {photos?.map(({ id, title, slug, image, description, categories, visible, user }) => (
@@ -29,6 +31,7 @@ const Photos = () => {
                         <Link to={`/photos/${slug}`} style={{ textDecoration: 'none', color: 'black' }}>
                             <PhotoCard
                                 title={title}
+                                slug={slug}
                                 image={image}
                                 description={description}
                                 categories={categories}
