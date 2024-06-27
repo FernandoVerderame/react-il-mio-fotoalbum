c1bd33f895ef0654b26f8cf9b657089f74c3d166
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-06-27 12:09:02 +0200

    added layout and homepage routes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,25 @@
+import { Route, Routes } from "react-router-dom";
+import DefaultLayout from "./layouts/DefaultLayout";
+import HomePage from "./pages/HomePage.jsx";
+
 function App() {
 
   return (
-    <>
-      <h1>App</h1>
-    </>
-  )
+    <Routes>
+
+      {/* Rotte pubbliche */}
+
+      {/* Default Layout */}
+      <Route path="/" element={<DefaultLayout />}>
+
+        {/* HomePage */}
+        <Route index element={<HomePage />} />
+
+
+      </Route>
+
+    </Routes>
+  );
 }
 
 export default App;
