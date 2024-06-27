0b43569755cd3e28576e79452f5019250f3f3d10
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-06-27 10:31:13 +0200

    added index, patch, destroy routes

--- Backend/routers/auth.js
+++ Backend/routers/auth.js
@@ -10,9 +10,18 @@ const validator = require("../middlewares/validator.js");
 // Importo le validazioni dell'users
 const { registerBody, loginBody } = require("../validations/users.js");
 
+// Autenticazione Token
+const authenticateToken = require('../middlewares/authToken.js');
+
+// Autenticazione SuperAdmin
+const superAdminPermission = require("../middlewares/authSuperAdmin.js");
+
 const {
     register,
-    login
+    login,
+    index,
+    patch,
+    destroy
 } = require("../controllers/auth.js");
 
 // Rotta registrazione
@@ -20,5 +29,17 @@ router.post('/register', validator(registerBody), register);
 
 // Rotta login 
 router.post('/login', validator(loginBody), login);
+
+// Validatore del Token e del Super Admin
+router.use('/users', [authenticateToken, superAdminPermission]);
+
+// Rotta Index
+router.get('/users', index);
+
+// Rotta Patch
+router.patch('/users/:email', patch);
+
+// Rotta Delete
+router.delete('/users/:email', destroy);
 
 module.exports = router;
