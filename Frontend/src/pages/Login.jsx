8d21728ee74c5119dfbb8dc4d18fa13eebd392d1
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-06-28 10:30:41 +0200

    updated login form

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -1,5 +1,7 @@
 import { useAuth } from "../contexts/AuthContext";
 import { useState } from "react";
+import { FaRegUserCircle as UserIcon } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Login = () => {
 
@@ -32,26 +34,53 @@ const Login = () => {
     }
 
     return (<>
-        <section id="login" className="d-flex justify-content-center mt-5">
-            <form onSubmit={handleLogin}>
-                <input
-                    type="email"
-                    placeholder="Email"
-                    value={formData.email}
-                    onChange={e => changeData('email', e.target.value)}
-                />
-                <input
-                    type="password"
-                    placeholder="Password"
-                    value={formData.password}
-                    onChange={e => changeData('password', e.target.value)}
-                />
-                {loginError !== null && <div className="text-danger">{loginError.message}</div>}
-                {loginError?.errors && loginError.errors.map((err, index) => (
-                    <div key={`err${index}`}>{err.msg}</div>
-                ))}
-                <button>Loggati</button>
-            </form>
+        <section id="login" className="d-flex justify-content-center align-items-center">
+            <div className="card w-25">
+                <div className="card-header">
+                    <div className="card-title text-center">
+                        <h1 className="h2 m-0 d-flex align-items-center justify-content-center gap-2"><UserIcon /><span>Login</span></h1>
+                    </div>
+                </div>
+                <div className="card-body">
+                    <form onSubmit={handleLogin}>
+                        <div className="mb-3">
+                            <label htmlFor="email" className="form-label h5">Email</label>
+                            <input
+                                id="email"
+                                name="email"
+                                type="email"
+                                placeholder="Email"
+                                value={formData.email}
+                                onChange={e => changeData('email', e.target.value)}
+                                className="form-control"
+                            />
+                        </div>
+
+                        <div className="mb-3">
+                            <label htmlFor="password" className="form-label h5">Password</label>
+                            <input
+                                id="password"
+                                name="password"
+                                type="password"
+                                placeholder="Password"
+                                value={formData.password}
+                                onChange={e => changeData('password', e.target.value)}
+                                className="form-control"
+                            />
+                        </div>
+
+                        {loginError !== null && <div className="text-danger">{loginError.message}</div>}
+                        {loginError?.errors && loginError.errors.map((err, index) => (
+                            <div key={`err${index}`}>{err.msg}</div>
+                        ))}
+
+                        <div className="d-flex align-items-center flex-column gap-2">
+                            <button className="btn btn-primary">Login</button>
+                            <Link to={'/register'}>Ancora non sei registrato? Registrati!</Link>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </section>
     </>)
 }
