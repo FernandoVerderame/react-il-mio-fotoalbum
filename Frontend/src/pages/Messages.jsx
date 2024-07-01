a86f990a18fa007cc43691dbb34b0108f53fc4db
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-07-01 11:30:18 +0200

    added messages delete

--- Frontend/src/pages/Messages.jsx
+++ Frontend/src/pages/Messages.jsx
@@ -1,9 +1,11 @@
 import axios from "../utils/axiosClient.js";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { MdDelete as DeleteBtn } from "react-icons/md";
+import { AiOutlineClose as CloseIcon } from "react-icons/ai";
 
 const Messages = () => {
 
+    // Fetch dei messaggi
     const [messages, setMessages] = useState([]);
 
     const fetchMessages = async () => {
@@ -16,6 +18,30 @@ const Messages = () => {
         fetchMessages();
     }, []);
 
+    // Cancellazione dei messaggi
+    const [messageToDelete, setMessageToDelete] = useState(null);
+
+    const deleteMessage = async () => {
+        if (messageToDelete) {
+            await axios.delete(`/messages/${messageToDelete}`);
+            setMessages(messages.filter(message => message.id !== messageToDelete));
+            setDeleteMode(false);
+        }
+    }
+
+    // Modale cancellazione
+    const [deleteMode, setDeleteMode] = useState(false);
+
+    const dialogRef = useRef();
+
+    useEffect(() => {
+        if (deleteMode) {
+            dialogRef.current.showModal();
+        } else {
+            dialogRef.current.close();
+        }
+    }, [deleteMode]);
+
     return (
         <section id="messages" className="container my-5">
             <div className="mb-5">
@@ -23,6 +49,22 @@ const Messages = () => {
             </div>
 
             <div className="d-flex justify-content-center">
+
+                {/* DeleteBtn */}
+                <dialog ref={dialogRef}>
+                    <div className="d-flex justify-content-between align-items-center">
+                        <h3>Sei sicuro?</h3>
+                        <CloseIcon onClick={() => setDeleteMode(false)} role='button' />
+                    </div>
+                    <p>Se procedi, eliminerai definitivamente il messaggio ricevuto.</p>
+                    <button
+                        onClick={deleteMessage}
+                        className="btn btn-danger btn-sm"
+                    >
+                        Elimina
+                    </button>
+                </dialog>
+
                 <table className="table table-hover table-dark w-75">
                     <thead>
                         <tr>
@@ -48,7 +90,13 @@ const Messages = () => {
                                     <td>{user.name}</td>
                                     <td>{user.email}</td>
                                     <td className="d-flex justify-content-end">
-                                        <button className="btn btn-sm btn-danger">
+                                        <button
+                                            onClick={() => {
+                                                setMessageToDelete(id);
+                                                setDeleteMode(true);
+                                            }}
+                                            className="btn btn-sm btn-danger"
+                                        >
                                             <DeleteBtn className="fs-5" />
                                         </button>
                                     </td>
@@ -58,7 +106,7 @@ const Messages = () => {
                     </tbody>
                 </table>
             </div>
-        </section>
+        </section >
     );
 }
 
