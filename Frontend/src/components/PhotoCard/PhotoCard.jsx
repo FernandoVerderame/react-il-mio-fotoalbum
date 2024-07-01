e6c4eba72a226a801f55fe1de7baf8cda29ab3e6
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-07-01 17:12:15 +0200

    fixed photo card

--- Frontend/src/components/PhotoCard/PhotoCard.jsx
+++ Frontend/src/components/PhotoCard/PhotoCard.jsx
@@ -15,10 +15,10 @@ const PhotoCard = ({ title, image, user }) => {
 
                 <div className={photoCardStyle.bottom}>
                     <div className="photo-title">
-                        <h3 className="h5">{title}</h3>
+                        <h3 className="h5 m-0">{title}</h3>
                     </div>
                     <div className="photo-author">
-                        <p className="fst-italic">{user?.name}</p>
+                        <p className="fst-italic m-0">{user?.name}</p>
                     </div>
                 </div>
             </div>
