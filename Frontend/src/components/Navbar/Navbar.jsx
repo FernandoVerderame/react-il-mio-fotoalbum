035d99cacd635afdf5b03f2c47657bc2c7ce47d5
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-07-01 14:53:45 +0200

    fixed navbar layout

--- Frontend/src/components/Navbar/Navbar.jsx
+++ Frontend/src/components/Navbar/Navbar.jsx
@@ -8,47 +8,49 @@ const Navbar = () => {
     const { isLoggedIn, logout, user } = useAuth();
 
     return (
-        <nav className="navbar navbar-expand-lg bg-black px-5">
-            <div className="container-fluid">
-                <div className="left-nav">
-                    <NavLink className="navbar-brand text-white d-flex align-items-center" to={'/'}>
-                        <img src={logo} alt="logo" className={navbarStyle.logo} />
-                    </NavLink>
-                </div>
-                <div className="center-nav">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0 fs-5 d-flex gap-3">
-                        <li className="nav-item">
-                            <NavLink className="nav-link text-white" to={'/'}>Home</NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink className="nav-link text-white" to={'/photos'}>Album</NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink className="nav-link text-white" to={'/categories'}>Categorie</NavLink>
-                        </li>
-                        {isLoggedIn &&
+        <header>
+            <nav className="navbar navbar-expand-lg bg-black px-5">
+                <div className="container-fluid">
+                    <div className="left-nav w-25">
+                        <NavLink className="navbar-brand text-white d-flex align-items-center" to={'/'}>
+                            <img src={logo} alt="logo" className={navbarStyle.logo} />
+                        </NavLink>
+                    </div>
+                    <div className="center-nav">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0 fs-5 d-flex gap-3">
                             <li className="nav-item">
-                                <NavLink className="nav-link text-white" to={'/messages'}>Messaggi</NavLink>
+                                <NavLink className="nav-link text-white" to={'/'}>Home</NavLink>
                             </li>
+                            <li className="nav-item">
+                                <NavLink className="nav-link text-white" to={'/photos'}>Album</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className="nav-link text-white" to={'/categories'}>Categorie</NavLink>
+                            </li>
+                            {isLoggedIn &&
+                                <li className="nav-item">
+                                    <NavLink className="nav-link text-white" to={'/messages'}>Messaggi</NavLink>
+                                </li>
+                            }
+                        </ul>
+                    </div>
+                    <div className="right-nav w-25 d-flex justify-content-end">
+                        {!isLoggedIn &&
+                            <div className="d-flex gap-2">
+                                <NavLink to={'/login'} className="btn btn-primary btn-sm">Login</NavLink>
+                                <NavLink to={'/register'} className="btn btn-secondary btn-sm">Register</NavLink>
+                            </div>
                         }
-                    </ul>
+                        {isLoggedIn &&
+                            <div className="d-flex align-items-center gap-2">
+                                <h3 className="m-0 fs-6 text-white">{user.name}</h3>
+                                <button onClick={logout} className="btn btn-secondary btn-sm">Logout</button>
+                            </div>
+                        }
+                    </div>
                 </div>
-                <div className="right-nav">
-                    {!isLoggedIn &&
-                        <div className="d-flex gap-2">
-                            <NavLink to={'/login'} className="btn btn-primary btn-sm">Login</NavLink>
-                            <NavLink to={'/register'} className="btn btn-secondary btn-sm">Register</NavLink>
-                        </div>
-                    }
-                    {isLoggedIn &&
-                        <div className="d-flex align-items-center gap-2">
-                            <h3 className="m-0 fs-6 text-white">{user.name}</h3>
-                            <button onClick={logout} className="btn btn-secondary btn-sm">Logout</button>
-                        </div>
-                    }
-                </div>
-            </div>
-        </nav>
+            </nav>
+        </header>
     );
 }
 
