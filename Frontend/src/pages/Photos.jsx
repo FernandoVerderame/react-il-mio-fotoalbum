da76b80d8dc1a19056d2e3e51eb10fd7bcbe0ea6
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-07-01 17:25:40 +0200

    added comments

--- Frontend/src/pages/Photos.jsx
+++ Frontend/src/pages/Photos.jsx
@@ -2,15 +2,18 @@ import axios from "../utils/axiosClient.js";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { MdFiberNew as AddPhoto } from "react-icons/md";
-import { FaSearch as SearchIcon } from "react-icons/fa";
 import PhotoCard from "../components/PhotoCard/PhotoCard.jsx";
+import SearchBar from "../components/SearchBar/SearchBar.jsx";
 
 const Photos = () => {
 
+    // useState delle foto
     const [photos, setPhotos] = useState([]);
 
+    // useState per la ricerca delle foto tramite il titolo
     const [searchTitle, setSearchTitle] = useState('');
 
+    // Fetch delle foto
     const fetchPhotos = async (title = '') => {
         const res = await axios.get(`/photos`, { params: { title } });
         const newPhotos = res.data.data;
@@ -21,6 +24,7 @@ const Photos = () => {
         fetchPhotos();
     }, []);
 
+    // onChange in tempo reale tramite la search-bar
     const handleSearch = (e) => {
         setSearchTitle(e.target.value);
         fetchPhotos(e.target.value);
@@ -30,30 +34,32 @@ const Photos = () => {
         <section className="container my-5">
             <div className="d-flex justify-content-between align-items-center mb-5">
                 <h1 className="m-0 text-white">Album</h1>
-                <div className="input-group search-bar">
-                    <span className="input-group-text">
-                        <SearchIcon />
-                    </span>
-                    <input
-                        type="text"
-                        className="form-control"
-                        placeholder="Username"
-                        value={searchTitle}
-                        onChange={handleSearch}
-                    />
-                </div>
+
+                {/* SearchBar */}
+                <SearchBar
+                    value={searchTitle}
+                    onChange={handleSearch}
+                />
+
+                {/* Aggiungi foto */}
                 <Link to="create" className="btn btn-primary d-flex align-items-center gap-1"><AddPhoto className="fs-5" /> Foto</Link>
             </div>
             <div className="row g-5">
                 {photos.length === 0 ? (
                     <div className="col-12">
+
+                        {/* Nel caso non ci siano foto */}
                         <p className="text-center text-white h3">Nessuna foto trovata!</p>
                     </div>
                 ) : (
                     photos.map(({ id, title, slug, image, description, categories, visible, user }) => (
                         visible === true &&
                         <div key={id} className="col-4">
+
+                            {/* Tasto show della singola foto */}
                             <Link to={`/photos/${slug}`} style={{ textDecoration: 'none', color: 'black' }}>
+
+                                {/* Card della foto */}
                                 <PhotoCard
                                     title={title}
                                     slug={slug}
