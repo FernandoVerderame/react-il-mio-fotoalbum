7fe8a8539ea515d8272ad931dc46fd013a47d164
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-07-01 18:57:40 +0200

    updated dumb modal

--- Frontend/src/components/Modal/Modal.jsx
+++ Frontend/src/components/Modal/Modal.jsx
@@ -1,15 +1,15 @@
 import { AiOutlineClose as CloseIcon } from "react-icons/ai";
 
-const DeleteModal = ({ dialogRef, setDeleteMode, deletePhoto, title }) => {
+const DeleteModal = ({ dialogRef, setDeleteMode, deleteBtn, title }) => {
     return (
         <dialog ref={dialogRef}>
             <div className="d-flex justify-content-between align-items-center">
                 <h3>Sei sicuro?</h3>
                 <CloseIcon onClick={() => setDeleteMode(false)} role='button' />
             </div>
-            <p>Se procedi, eliminerai definitivamente la foto con titolo: "{title}".</p>
+            <p>Se procedi, eliminerai definitivamente: "{title}".</p>
             <button
-                onClick={deletePhoto}
+                onClick={deleteBtn}
                 className="btn btn-danger btn-sm"
             >
                 Elimina
