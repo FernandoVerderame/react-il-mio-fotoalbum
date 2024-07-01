a121925f413fe21ec77acbf984145bc29e629afa
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-07-01 14:53:23 +0200

    fixed footer

--- Frontend/src/components/Footer/Footer.jsx
+++ Frontend/src/components/Footer/Footer.jsx
@@ -1,9 +1,11 @@
+import footerStyle from './Footer.module.scss';
+
 const Footer = () => {
     return (
         <>
             <footer className="bg-black">
                 <div className="container">
-                    <div className="text-white py-3">Made by <i>Fernando Verderame</i>.</div>
+                    <div className="text-white">Made by <i>Fernando Verderame</i>.</div>
                 </div>
             </footer>
         </>
