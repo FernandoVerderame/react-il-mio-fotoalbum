57bf8a81bc4914cc367a6544160ab0d445d32ed0
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-07-01 16:16:02 +0200

    added auth admin into categories router

--- Backend/routers/categories.js
+++ Backend/routers/categories.js
@@ -16,6 +16,9 @@ const paramID = require('../validations/generic.js');
 // Autenticazione Token
 const authenticateToken = require('../middlewares/authToken.js');
 
+// Autenticazione Admin
+const adminPermission = require("../middlewares/authAdmin.js");
+
 // Importo le funzioni delle categorie
 const {
     store,
@@ -30,6 +33,9 @@ router.get('/', index);
 
 // ? Rotte Protette
 router.use(authenticateToken);
+
+// ! Rotte Admin
+router.use(adminPermission);
 
 // Rotta store
 router.post('/', validator(bodyData), store);
