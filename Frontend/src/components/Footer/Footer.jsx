621936fd5899cfa82a0e928adaa62320dd546f9f
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-07-01 10:33:29 +0200

    added footer

---
+++ Frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <>
+            <footer className="bg-black">
+                <div className="container">
+                    <div className="text-white py-3">Made by <i>Fernando Verderame</i>.</div>
+                </div>
+            </footer>
+        </>
+    );
+}
+
+export default Footer;
--- Frontend/src/layouts/DefaultLayout.jsx
+++ Frontend/src/layouts/DefaultLayout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar/Navbar.jsx";
+import Footer from "../components/Footer/Footer.jsx";
 
 const DefaultLayout = () => {
     return (
         <>
             <Navbar />
             <Outlet />
+            <Footer />
         </>
     );
 }
