20c41e9980eee52e69943c2ad6c0897c55844729
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-07-03 16:56:20 +0200

    added social icons footer

--- Frontend/src/components/Footer/Footer.jsx
+++ Frontend/src/components/Footer/Footer.jsx
@@ -1,11 +1,35 @@
 import footerStyle from './Footer.module.scss';
+import { FaInstagram } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
 
 const Footer = () => {
     return (
         <>
             <footer className="bg-black">
-                <div className="container">
+                <div className="container d-flex align-items-center justify-content-between">
                     <div className="text-white">Made by <i>Fernando Verderame</i>.</div>
+                    <ul className="d-flex list-unstyled gap-3 m-0 fs-4">
+                        <li>
+                            <a
+                                href="https://www.instagram.com/_fernandoverderame_/"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="text-white"
+                            >
+                                <FaInstagram />
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                href="https://www.linkedin.com/in/fernando-verderame-smm/"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="text-white"
+                            >
+                                <FaLinkedin />
+                            </a>
+                        </li>
+                    </ul>
                 </div>
             </footer>
         </>
